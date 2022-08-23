607b0b31cb9e695356e2876796b6366476a476aa
sarmiente <carlosarmientopinto@gmail.com>
2022-08-23 17:07:13 -0500

    2 files modified, 1 added, 1 deleted

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,22 +1,26 @@
 const VideoPlayer = () => {
   return (
-    <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
-      <div class="relative z-10 p-5 text-2xl text-white bg-red-300 bg-opacity-50 rounded-xl">
-        Contactar
-      </div>
-      <video
-        autoplay
-        loop
-        muted
-        class="absolute w-auto min-w-full min-h-full max-w-none"
-      >
-        <source
+    <section
+      id="Inicio"
+      className="relative h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3"
+    >
+      <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+        <video
+          className="min-w-full min-h-full absolute object-cover"
           src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
           type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
-    </header>
+          autoPlay
+          muted
+          loop
+        ></video>
+      </div>
+      <div className="video-content space-y-2 z-10">
+        <h1 className="font-light font-sans text-6xl">Lotes de 1000m²</h1>
+        <h3 className="font-light font-display text-3xl">
+          IDEALES PARA CASA FINCAS
+        </h3>
+      </div>
+    </section>
   );
 };
 
